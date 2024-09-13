d8fc076a220b62dac4e9f4d57355cfdabec81e66
Samuel Heal <samuelheal@Samuels-Laptop.local>
2024-09-13 10:13:35 +0930

    nav bar styling draft complete

--- inc/floor_menu.inc.php
+++ inc/floor_menu.inc.php
@@ -1,7 +1,23 @@
-<ul id="menu">
+<!-- <ul id="navbar">
     <li><a href="factory.php">Factory</a></li>
     <li><a href="jobs.php">Jobs</a></li>
     <li><a href="machines.php">Machines</a></li>
     <li><a href="messages.php">Messages</a></li>
     <li><a href="">Logout</a></li>
-</ul>
+</ul> -->
+
+<div class="headerContainer">
+    <div class="username">
+        <p>User name</p>
+    </div>
+    <div class="navButtons">
+        <div id="menu-items">
+            <a class="menu-item" href="factory.php">Factory</a>
+            <a class="menu-item" href="jobs.php">Jobs</a>
+            <a class="menu-item" href="machines.php">Machines</a>
+            <a class="menu-item" href="messages.php">Messages</a>
+            <a class="menu-item" href="">Logout</a>
+        </div>
+        
+    </div>
+</div>
