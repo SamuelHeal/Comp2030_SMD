43ad69760b50c26de6266068ca18e212a288ce00
Samuel Heal <samuelheal@Samuels-Laptop.local>
2024-09-13 09:30:42 +0930

    fixed sql errors in database schema

--- db.sql
+++ db.sql
@@ -6,7 +6,7 @@ CREATE DATABASE Group_18_SMD;
 USE Group_18_SMD;
 
 CREATE TABLE Users(
-    id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id int NOT NULL AUTO_INCREMENT,
     firstname varchar(100) NOT NULL,
     lastname varchar(100) NOT NULL,
     dob varchar(100) NOT NULL,
@@ -15,17 +15,15 @@ CREATE TABLE Users(
     email varchar(100) NOT NULL,
     employmentdate varchar(100) NOT NULL,
     pin varchar(100) NOT NULL,
-    PRIMARY KEY (id),
+    PRIMARY KEY (id)
 ) AUTO_INCREMENT = 1;
 
 CREATE TABLE Jobs(
-    id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id int NOT NULL AUTO_INCREMENT,
     OperatorID int,
-    FOREIGN KEY (OperatorID) REFERENCES User(id),
+    FOREIGN KEY (OperatorID) REFERENCES Users(id),
     PRIMARY KEY (id)
-)
-
-INSERT INTO Users(firstname, lastname, dob, position, phonenumber, email, employmentdate, pin) VALUES('Sam', 'Heal', '06/08/1999', 'Factory Manager', '04111111111', 'heal0163@flinders.edu.au', '12/09/2024', '0000');
+);
 
 CREATE user IF NOT EXISTS dbadmin@localhost;
 GRANT all privileges ON Group_18_SMD.Users TO dbadmin@localhost;
