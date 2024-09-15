830c9644c549bfb8317b323fa0fc76811f6dbc06
Samuel Heal <samuelheal@Samuels-Laptop.modem>
2024-09-15 09:47:37 +0930

    committing to merge in main

--- createusersystem.php
+++ createusersystem.php
@@ -25,18 +25,3 @@
 } else {
     echo "Failed to create user"; 
 }
-
-// if (isset($_POST["task-name"])) { 
-//         require_once "inc/dbconn.inc.php";
-//         $sql = "INSERT INTO Task(name) VALUES(?);" ;
-//         $statement = mysqli_stmt_init($conn);
-//         mysqli_stmt_prepare($statement, $sql); 
-//         mysqli_stmt_bind_param($statement, 's', htmlspecialchars($_POST["task-name"]));
-        // if (mysqli_stmt_execute($statement)) {
-        //     header("location: index.php"); 
-        // } else {
-        //     mysqli_error($conn);
-        // }
-        // mysqli_close($conn);
-//     }
-// 
