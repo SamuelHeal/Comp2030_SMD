44e9fca14e93eb82afe48ffc568291ac248106a2
ArranWill <arranwill@proton.me>
2024-09-15 19:30:28 +0930

    Insert statements for six people

---
+++ insert-persons.sql
@@ -0,0 +1,65 @@
+INSERT INTO Person (firstName, lastName, DOB, email, employmentDate, phoneNumber, position, PIN) VALUES(
+    "Frank",
+    "Colson",
+    "1977-04-14",
+    "frank.colson@gmail.com",
+    "2010-03-01",
+    "0412978234",
+    "Factory Manager",
+    "$2y$10$koWdFZA8O8su.jrxKWgmSedcB5/Svh.uyRZGVquY3ERVv1ZPoi55S"  -- "4545"
+);
+
+INSERT INTO Person (firstName, lastName, DOB, email, employmentDate, phoneNumber, position, PIN) VALUES(
+    "Robert",
+    "McKenna",
+    "1994-07-20",
+    "robert.mckenna@icloud.com",
+    "2019-09-01",
+    "0487098124",
+    "Production Operator",
+    "$2y$10$GEW1/xVj6CJUU0UYBI5k6O19cug7sZSxKgEnU/Y7y0bFBEFMYMIRm"  -- "9900"
+);
+
+INSERT INTO Person (firstName, lastName, DOB, email, employmentDate, phoneNumber, position, PIN) VALUES(
+    "Brian",
+    "Moser",
+    "1964-12-03",
+    "brian.moser@iinet.com",
+    "2002-07-01",
+    "0409925781",
+    "Production Operator",
+    "$2y$10$5Y3gAAMGFE5FkVoTA0G.LexB0/yEcbXYjIWv1X9D.CDHd03Oh3tbu"  -- "1224"
+);
+
+INSERT INTO Person (firstName, lastName, DOB, email, employmentDate, phoneNumber, position, PIN) VALUES(
+    "Timothy",
+    "Newman",
+    "2000-4-07",
+    "timmy.newman@gmail.com",
+    "2022-03-01",
+    "0489766782",
+    "Production Operator",
+    "$2y$10$6XBFXdi/ELW7VX59XhKVh..Zl0hu710cCqY5jS4hlGu5JWNVvaP5u"  -- "5651"
+);
+
+INSERT INTO Person (firstName, lastName, DOB, email, employmentDate, phoneNumber, position, PIN) VALUES(
+    "Jack",
+    "Dennis",
+    "1972-06-29",
+    "j.dennis@bigpond.com",
+    "2008-05-01",
+    "0444097931",
+    "Auditor",
+    "$2y$10$K.GBltHkRxVOHoVrVJ6OBeptV/NMh17e8bG5UdW1CtefPF/rLpnxK"  -- "1839"
+);
+
+INSERT INTO Person (firstName, lastName, DOB, email, employmentDate, phoneNumber, position, PIN) VALUES(
+    "Richard",
+    "Moore",
+    "1989-03-06",
+    "richard.moore@gmail.com",
+    "2021-09-01",
+    "0409244751",
+    "Administrator",
+    "$2y$10$qOS3FF82H8sSIC1AC9rF3.Exg/ABG9tMUtj44Gy1pX4vXWz45sLYK"  -- "0741"
+);
