9ef485a40bb7bbdfd9d4073221f9014f4474218f
Samuel Heal <samuelheal@Samuels-Laptop.modem>
2024-09-15 09:41:09 +0930

    pushing to git

--- db.sql
+++ db.sql
@@ -1,9 +1,9 @@
 SET @@AUTOCOMMIT = 1;
 
-DROP DATABASE IF EXISTS Group_18_SMD;
-CREATE DATABASE Group_18_SMD;
+DROP DATABASE IF EXISTS Group18_SMD;
+CREATE DATABASE Group18_SMD;
 
-USE Group_18_SMD;
+USE Group18_SMD;
 
 /* Table Declarations */
 CREATE TABLE Machine(
@@ -23,7 +23,7 @@ CREATE TABLE Person(
     employmentDate VARCHAR(100) NOT NULL,
     phoneNumber VARCHAR(100) NOT NULL,
     position VARCHAR(100) NOT NULL,
-    PIN VARCHAR(255) NOT NULL,
+    PIN INTEGER NOT NULL,
     PRIMARY KEY (personID)
 );
 
@@ -67,20 +67,22 @@ CREATE TABLE Note(
     FOREIGN KEY (managerID) REFERENCES Person(personID)
 );
 
-CREATE TABLE Log(
-    machineID INTEGER NOT NULL AUTO_INCREMENT,
-    timestamp datetime NOT NULL,
-    operationalStatus VARCHAR(20) NOT NULL,
-    maintenanceLog VARCHAR(100),
-    errorCode VARCHAR(5),
-    productionCount INTEGER NOT NULL,
-    humidity FLOAT,
-    powerConsumption FLOAT,
-    pressure FLOAT,
-    speed FLOAT,
+
+CREATE TABLE Log (
+    timestamp DATETIME NOT NULL,
+    machineID INT NOT NULL,
+    machine_name VARCHAR(100) NOT NULL,
     temperature FLOAT,
+    pressure FLOAT,
     vibration FLOAT,
-    PRIMARY KEY (machineID, timestamp),
+    humidity FLOAT,
+    power_consumption FLOAT,
+    operational_status VARCHAR(20) NOT NULL,
+    error_code VARCHAR(5),
+    production_count INT,
+    maintenance_log TEXT,
+    speed FLOAT,
+    PRIMARY KEY (timestamp, machineID),
     FOREIGN KEY (machineID) REFERENCES Machine(machineID)
 );
 
@@ -103,87 +105,3 @@ GRANT ALL PRIVILEGES ON Group_18_SMD.Message TO dbadmin@localhost;
 GRANT ALL PRIVILEGES ON Group_18_SMD.Note TO dbadmin@localhost;
 GRANT ALL PRIVILEGES ON Group_18_SMD.Log TO dbadmin@localhost;
 GRANT ALL PRIVILEGES ON Group_18_SMD.Part TO dbadmin@localhost;
-
-/* Insert Statements */
-INSERT INTO Machine VALUES(
-    999,
-    "3D Printer",
-    "B0",
-    2
-);
-
-INSERT INTO Person VALUES(
-    1,
-    "Frank",
-    "Colson",
-    "10/08/1978 00:00",
-    "frank.colson@bigpond.com.au",
-    "01/02/2010",
-    "0489780234",
-    "Manager",
-    4545
-);
-
-INSERT INTO Person VALUES(
-    2,
-    "Robert",
-    "McKenna",
-    "14/12/1990 00:00",
-    "robmckenna@messaging.com.au",
-    "01/08/2019",
-    "0412546802",
-    "Production Operator",
-    9900
-);
-
-INSERT INTO Job VALUES(
-    2,
-    "There's a problem with the 3D printer. Please see what the problem is and fix it.",
-    999,
-    2,
-    4,
-    NOW()
-);
-
-INSERT INTO Message VALUES(
-    5,
-    NOW(),
-    2,
-    1,
-    2,
-    "Building Maintenance",
-    "The tap nearest the door in the gent's bathroom doesn't work"
-);
-
-INSERT INTO Note VALUES(
-    9,
-    2,
-    "Issue with Parts/Materials",
-    "Parts Running Low",
-    "PLA Filament [F1001]",
-    1,
-    3,
-    "Lorem ipsum dolor"
-);
-
-INSERT INTO Log VALUES(
-    999,
-    "2024-04-01 00:00:00",
-    "active",
-    NULL,
-    NULL,
-    96,
-    36.76,
-    330.36,
-    3.16,
-    3.35,
-    71.18,
-    0.24
-);
-
-INSERT INTO Part VALUES(
-    1001,
-    999,
-    "PLA Filament [F1001]",
-    "Part for the 3D printer."
-);
